d2c61f973692065c7f600987aba8836885d7f94f
Yun4g <delightvincent487@gmail.com>
2025-08-19 12:06:43 +0100

    push update

--- src/app/Dashboard/RefeeralDashboard/referals/[id]/page.tsx
+++ src/app/Dashboard/RefeeralDashboard/referals/[id]/page.tsx
@@ -1,6 +1,9 @@
-import ReferralClient from "../referralClient";
+import ReferralClient from '../referralClient';
 
+interface PageProps {
+  params: { id: string };
+}
 
-export default function ReferralDetailsPage({ params }: { params: { id: string } }) {
+export default function ReferralDetailsPage({ params }: PageProps) {
   return <ReferralClient id={params.id} />;
 }
